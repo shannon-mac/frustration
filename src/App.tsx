7a57e1f7519031aea97a717fdf73bf28f31ef0c7
Shannon Macdonald <shannon1@ca.ibm.com>
2026-07-02 16:19:46 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
 import { GameBoard } from './components/GameBoard/GameBoard';
 import { GameSetup } from './components/GameSetup/GameSetup';
 import { useGameState } from './hooks/useGameState';
 
 export default function App() {
   const game = useGameState();
-  const [playerCount, setPlayerCount] = useState(3);
 
   function handleStart(n: number) {
-    setPlayerCount(n);
     game.startGame(n);
   }
 
